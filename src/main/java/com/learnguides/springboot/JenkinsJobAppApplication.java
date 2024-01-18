bc7c8c765e642e5dc5d881f78cbd7bc48e1b7d1f
Ahamed Riaz <63754265+ahamedriaz@users.noreply.github.com>
2024-01-18 23:07:49 +0530

    First Commit - DevOps

--- src/main/java/com/learnguides/springboot/JenkinsJobAppApplication.java
+++ src/main/java/com/learnguides/springboot/JenkinsJobAppApplication.java
@@ -5,23 +5,20 @@ import org.apache.juli.logging.LogFactory;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsJobAppApplication {
-
-    public static final Logger logger = (Logger) LoggerFactory.getLogger(JenkinsJobAppApplication.class);
-
-    @PostConstruct
-    public void init() {
-        logger.info("Welcome To Learner Guides");
-
-        System.out.println("Welcome To Learner Guides");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsJobAppApplication.class, args);
+    }
+
+    @GetMapping("/get")
+    public String getMessage() {
+        return "Welcome To DevOps....";
     }
 
 }
